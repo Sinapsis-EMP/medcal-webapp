a7305e6ed86c4bf814aa27f28036f0ca0d8b9e6a
David Regla <dreglad@gmail.com>
2020-08-14 22:38:59 -0500

    Normailzar con Prettier

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-A starter template for [SvelteFire](https://github.com/codediodeio/sveltefire). 
+A starter template for [SvelteFire](https://github.com/codediodeio/sveltefire).
 
 ## 1. Degit the Template
 
@@ -14,11 +14,10 @@ Create a Firebase project at https://firebase.google.com/ and grab your web conf
 
 ![](https://firebasestorage.googleapis.com/v0/b/firestarter-96e46.appspot.com/o/project-config.PNG?alt=media&token=5eabb205-7ba2-4fc3-905f-e9547055e754)
 
-Opt-in to the following services from the Firebase console to run the demo. 
+Opt-in to the following services from the Firebase console to run the demo.
 
-1. **Anonymous** Login under *authentication -> sign-in method*
-1. **Cloud Firestore** under *database*. Make sure it's in test mode (or provide write access to the `posts/` collection using Security Rules).  
-
+1. **Anonymous** Login under _authentication -> sign-in method_
+1. **Cloud Firestore** under _database_. Make sure it's in test mode (or provide write access to the `posts/` collection using Security Rules).
 
 ## 3. Update the Firebase Config
 
@@ -40,4 +39,4 @@ You should see something like this:
 
 ![sveltefire demo app](https://firebasestorage.googleapis.com/v0/b/sveltefire-testing.appspot.com/o/sveltefire-demo.gif?alt=media&token=d5ea2807-7c50-4f94-bc73-8698b9528902)
 
-*Forked from the official --> [sveltejs/template](https://github.com/sveltejs/component-template)*
+_Forked from the official --> [sveltejs/template](https://github.com/sveltejs/component-template)_
--- src/App.svelte
+++ src/App.svelte
@@ -1,122 +1,3 @@
-<script>
-  import { FirebaseApp, User, Doc, Collection } from "sveltefire";
-
-  import firebase from "firebase/app";
-  import "firebase/firestore";
-  import "firebase/auth";
-  import "firebase/performance";
-  import "firebase/analytics";
-
-  let firebaseConfig = {
-  // Insert Firebase Credentials here
-  };
-
-  firebase.initializeApp(firebaseConfig);
-</script>
-
-<main>
-
-  {#if !firebaseConfig.projectId}
-    <strong>Step 0</strong>
-    Create a
-    <a href="https://firebase.google.com/">Firebase Project</a>
-    and paste your web config into
-    <code>App.svelte</code>
-    .
-  {/if}
-
-  <!-- 1. 🔥 Firebase App -->
-  <FirebaseApp {firebase}>
-
-    <h1>💪🔥 Mode Activated</h1>
-
-    <p>
-      <strong>Tip:</strong>
-      Open the browser console for development logging.
-    </p>
-
-
-    <!-- 2. 😀 Get the current user -->
-    <User let:user let:auth>
-      Howdy 😀! User
-      <em>{user.uid}</em>
-
-      <button on:click={() => auth.signOut()}>Sign Out</button>
-
-      <div slot="signed-out">
-
-        <button on:click={() => auth.signInAnonymously()}>
-          Sign In Anonymously
-        </button>
-      </div>
-
-      <hr />
-
-      <!-- 3. 📜 Get a Firestore document owned by a user -->
-      <Doc path={`posts/${user.uid}`} let:data={post} let:ref={postRef} log>
-
-        <h2>{post.title}</h2>
-
-        <p>
-          Document
-          created at <em>{new Date(post.createdAt).toLocaleString()}</em>
-        </p>
-
-        <span slot="loading">Loading post...</span>
-        <span slot="fallback">
-          <button
-            on:click={() => postRef.set({
-                title: '📜 I like Svelte',
-                createdAt: Date.now()
-              })}>
-            Create Document
-          </button>
-        </span>
-
-        <!-- 4. 💬 Get all the comments in its subcollection -->
-
-        <h3>Comments</h3>
-        <Collection
-          path={postRef.collection('comments')}
-          query={ref => ref.orderBy('createdAt')}
-          let:data={comments}
-          let:ref={commentsRef}
-          log>
-
-          {#if !comments.length}
-              No comments yet...
-          {/if}
-
-          {#each comments as comment}
-            <p>
-              <!-- ID: <em>{comment.ref.id}</em> -->
-            </p>
-            <p>
-              {comment.text}
-              <button on:click={() => comment.ref.delete()}>Delete</button>
-            </p>
-          {/each}
-
-
-          <button
-            on:click={() => commentsRef.add({
-                text: '💬 Me too!',
-                createdAt: Date.now()
-              })}>
-            Add Comment
-          </button>
-
-          <span slot="loading">Loading comments...</span>
-
-        </Collection>
-      </Doc>
-    </User>
-  </FirebaseApp>
-
-</main>
-
-
-<!-- Styles -->
 <style>
   main {
     text-align: center;
@@ -142,3 +23,133 @@
     }
   }
 </style>
+
+<script>
+  import { FirebaseApp, User, Doc, Collection } from "sveltefire";
+
+  import firebase from "firebase/app";
+  import "firebase/firestore";
+  import "firebase/auth";
+  import "firebase/performance";
+  import "firebase/analytics";
+
+  let firebaseConfig = {
+    apiKey: "AIzaSyDLjnjDgQe92lXVasJm_S7BXEzgOwFWUfM",
+    authDomain: "marco-6beb1.firebaseapp.com",
+    databaseURL: "https://marco-6beb1.firebaseio.com",
+    projectId: "marco-6beb1",
+    storageBucket: "marco-6beb1.appspot.com",
+    messagingSenderId: "877543684199",
+    appId: "1:877543684199:web:f382336f906065948bbcc2",
+    measurementId: "G-S1TV8D7S1H",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+<main>
+
+  {#if !firebaseConfig.projectId}
+    <strong>Step 0</strong>
+    Create a
+    <a href="https://firebase.google.com/">Firebase Project</a>
+    and paste your web config into
+    <code>App.svelte</code>
+    .
+  {/if}
+
+  <!-- 1. 🔥 Firebase App -->
+  <FirebaseApp firebase="firebase">
+
+    <h1>💪🔥 Mode Activated</h1>
+
+    <p>
+      <strong>Tip:</strong>
+      Open the browser console for development logging.
+    </p>
+
+    <!-- 2. 😀 Get the current user -->
+    <User let:user let:auth>
+      Howdy 😀! User
+      <em>{user.uid}</em>
+
+      <button on:click="{() => auth.signOut()}">Sign Out</button>
+
+      <div slot="signed-out">
+
+        <button on:click="{() => auth.signInAnonymously()}">
+          Sign In Anonymously
+        </button>
+      </div>
+
+      <hr />
+
+      <!-- 3. 📜 Get a Firestore document owned by a user -->
+      <Doc
+        path="{`posts/${user.uid}`}"
+        let:data="{post}"
+        let:ref="{postRef}"
+        log
+      >
+
+        <h2>{post.title}</h2>
+
+        <p>
+          Document created at
+          <em>{new Date(post.createdAt).toLocaleString()}</em>
+        </p>
+
+        <span slot="loading">Loading post...</span>
+        <span slot="fallback">
+          <button
+            on:click="{() => postRef.set({
+                title: '📜 I like Svelte',
+                createdAt: Date.now(),
+              })}"
+          >
+            Create Document
+          </button>
+        </span>
+
+        <!-- 4. 💬 Get all the comments in its subcollection -->
+
+        <h3>Comments</h3>
+        <Collection
+          path="{postRef.collection('comments')}"
+          query="{(ref) => ref.orderBy('createdAt')}"
+          let:data="{comments}"
+          let:ref="{commentsRef}"
+          log
+        >
+
+          {#if !comments.length}No comments yet...{/if}
+
+          {#each comments as comment}
+            <p>
+              <!-- ID: <em>{comment.ref.id}</em> -->
+            </p>
+            <p>
+              {comment.text}
+              <button on:click="{() => comment.ref.delete()}">Delete</button>
+            </p>
+          {/each}
+
+          <button
+            on:click="{() => commentsRef.add({
+                text: '💬 Me too!',
+                createdAt: Date.now(),
+              })}"
+          >
+            Add Comment
+          </button>
+
+          <span slot="loading">Loading comments...</span>
+
+        </Collection>
+      </Doc>
+    </User>
+  </FirebaseApp>
+
+</main>
+
+<!-- Styles -->
